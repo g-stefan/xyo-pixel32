4c71641b3816d1cd53f46e0fe5565831a2faab1b
Grigore Stefan <g_stefan@yahoo.com>
2021-08-09 15:41:58 +0300

    Update

---
+++ build/msvc.version-dependency-update.cmd
@@ -0,0 +1,8 @@
+@echo off
+rem Public domain
+rem http://unlicense.org/
+rem Created by Grigore Stefan <g_stefan@yahoo.com>
+
+echo -^> version-dependency-update xyo-pixel32
+
+xyo-cc --update-version-dependency xyo-pixel32
--- source/xyo-pixel32-version.cpp
+++ source/xyo-pixel32-version.cpp
@@ -15,9 +15,9 @@ namespace XYO {
 		namespace Version {
 
 			static const char *version_ = "1.7.0";
-			static const char *build_ = "9";
-			static const char *versionWithBuild_ = "1.7.0.9";
-			static const char *datetime_ = "2021-07-23 16:34:02";
+			static const char *build_ = "10";
+			static const char *versionWithBuild_ = "1.7.0.10";
+			static const char *datetime_ = "2021-08-05 20:38:15";
 
 			const char *version() {
 				return version_;
