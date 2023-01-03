a71b192bcaff056dd614c10c625e13552018925a
Grigore Stefan <g_stefan@yahoo.com>
2023-01-03 21:14:31 +0200

    Update

--- fabricare/make.prepare.js
+++ fabricare/make.prepare.js
@@ -3,13 +3,8 @@
 // SPDX-FileCopyrightText: 2022 Grigore Stefan <g_stefan@yahoo.com>
 // SPDX-License-Identifier: Unlicense
 
-messageAction("make");
+messageAction("make.prepare");
 
 exitIf(fileToCS("--touch=source/XYO/Pixel32/License.cpp", "--file-in=source/XYO/Pixel32/License/xyo-pixel32.txt", "--file-out=source/XYO/Pixel32/License.xyo-pixel32.Source.cpp", "--is-string-direct"));
 exitIf(fileToCS("--touch=source/XYO/Pixel32/License.cpp", "--file-in=source/XYO/Pixel32/License/libpng.txt", "--file-out=source/XYO/Pixel32/License.libpng.Source.cpp", "--is-string-direct"));
 exitIf(fileToCS("--touch=source/XYO/Pixel32/License.cpp", "--file-in=source/XYO/Pixel32/License/zlib.txt", "--file-out=source/XYO/Pixel32/License.zlib.Source.cpp", "--is-string-direct"));
-
-if (!Fabricare.include("make." + Project.make)) {
-	messageError("Don't know how to make '" + Project.make + "'!");
-	exit(1);
-};
