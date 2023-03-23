a6d59a0ee3be68051da9a49a6549b2ec4843d264
Grigore Stefan <g_stefan@yahoo.com>
2023-03-23 22:50:25 +0200

    Update

--- source/XYO/Pixel32/Copyright.cpp
+++ source/XYO/Pixel32/Copyright.cpp
@@ -14,19 +14,19 @@ namespace XYO::Pixel32::Copyright {
 	static const char *company_ = XYO_PIXEL32_COMPANY;
 	static const char *contact_ = XYO_PIXEL32_CONTACT;
 
-	std::string copyright() {
+	const char *copyright() {
 		return copyright_;
 	};
 
-	std::string publisher() {
+	const char *publisher() {
 		return publisher_;
 	};
 
-	std::string company() {
+	const char *company() {
 		return company_;
 	};
 
-	std::string contact() {
+	const char *contact() {
 		return contact_;
 	};
 
